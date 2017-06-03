f45e5ac73e114cc96c84d677c4d69c5da7c2eac5
TheRetiredToddler <valeerick55@gmail.com>
2017-06-03 03:56:40 +0000

    Update collect.py

--- collect.py
+++ collect.py
@@ -6,26 +6,26 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument("url", type=str)
-parser.add_argument("--wt", type=str)
+parser.add_argument("-wt", action="store_true")
 
 args = parser.parse_args()
 
 def getPlaylistLinks(url, withTitle):
-	sourceCode = requests.get(url).text
-	soup = BeautifulSoup(sourceCode, 'html.parser')
-	domain = "https://www.youtube.com"
-	
-	sys.stdout.write("\n")
-	for link in soup.find_all("a", {"dir": "ltr"}):
-		href = link.get("href")
-		videoTitle = link.string.strip()
-		
-		if args.wt is 't':
-			if href.startswith("/watch?"):
-				sys.stdout.write(videoTitle + '\n')
-				sys.stdout.write(domain + href + '\n\n')
-		if args.wt is None:
-			if href.startswith("/watch?"):
-				sys.stdout.write(domain + href + '\n')
-				
+        sourceCode = requests.get(url).text
+        soup = BeautifulSoup(sourceCode, 'html.parser')
+        domain = "https://www.youtube.com"
+
+        sys.stdout.write("\n")
+        for link in soup.find_all("a", {"dir": "ltr"}):
+                href = link.get("href")
+                videoTitle = link.string.strip()
+
+                if args.wt:
+                        if href.startswith("/watch?"):
+                                sys.stdout.write(videoTitle + '\n')
+                                sys.stdout.write(domain + href + '\n\n')
+                else:
+                        if href.startswith("/watch?"):
+                                sys.stdout.write(domain + href + '\n')
+
 getPlaylistLinks(args.url, args.wt)
